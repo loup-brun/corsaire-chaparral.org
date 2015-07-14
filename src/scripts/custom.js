71732bbde7a061c5e97ea1cde446731ac229623a
Louis-Olivier Brassard <louis.oli.br@gmail.com>
2015-07-14 15:23:35 -0400

    improved dropdowns, with touch support, +css fixes on sidebar

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -3,12 +3,6 @@
 
   head.ready(document, function() {
 
-    // small utility to detect touch support
-    function isTouch() {
-      return 'ontouchstart' in win  ||
-        'onmsgesturechange' in win;
-    }
-
     // include qwery as a selector engine
     bean.setSelectorEngine(qwery);
 
@@ -19,7 +13,7 @@
 
     function toggleNav(handle, navElem) {
 
-      bean.on(handle, 'click', function(event) {
+      bean.on(handle, 'click touchstart', function(event) {
 
         if (classie.has(navElem, 'toggled')) {
           Velocity(navElem, { translateX: [260, 0] }, { duration: 400, easing: 'easeInCubic', display: 'none' });
@@ -38,32 +32,70 @@
 
     // Prevent default on touch screens for dropdowns
 
-    var dropdowns = qwery('.dropdown');
+    var navbar = qwery('.navbar-main')[0],
+        dropdowns = qwery('.dropdown', navbar),
+        nestedDropdowns = qwery('.dropdown .dropdown', navbar);
 
     function catchDropdown(ddown) {
-      var counter = 0;
 
-      var a = qwery('a', ddown)[0];
+      function isNestedDropdown(ddown) {
+        var ret = false;
 
-      bean.on(a, 'click', function(e) {
+        for (var j in nestedDropdowns) {
+          if (ddown === nestedDropdowns[j]) {
+            ret = true;
+          }
+        }
+
+        return ret;
+      }
+
+      function clearAllDropdowns() {
         for (var i in dropdowns) {
           classie.remove(dropdowns[i], 'open');
         }
-        
-        if (!counter % 2) {
+      }
+
+      var a = qwery('a', ddown)[0];
+
+      bean.on(a, 'click touchstart', function(e) {
+
+        if (!classie.hasClass(ddown, 'open')) {
           e.preventDefault();
-          classie.add(ddown, 'open');
+          for (var i in dropdowns) {
+
+            // Don't cancel parent dropdowns
+            if (isNestedDropdown(ddown)) {
+              var parent = ddown.parentElement;
+
+              // Reach the parent dropdown
+              while (!classie.has(parent, 'dropdown')) {
+                parent = parent.parentElement;
+              }
+
+              // If this is not a parent of a child dropdown,
+              // close the dropdown
+              if ( dropdowns[i] !== parent) {
+                classie.remove(dropdowns[i], 'open');
+              }
+            } else {
+              classie.remove(dropdowns[i], 'open');
+            }
+          }
+          // Ensure the class is added after closing all other dropdowns
+          win.setTimeout(function() {
+            classie.add(ddown, 'open');
+          }, 100);
         }
 
-        counter ++;
       });
     }
 
-    if (isTouch()) {
-      for (var k in dropdowns) {
-        catchDropdown(dropdowns[k]);
-      }
+    //if (head.touch()) {
+    for (var k in dropdowns) {
+      catchDropdown(dropdowns[k]);
     }
+    //}
 
   });
 })(window, document);
